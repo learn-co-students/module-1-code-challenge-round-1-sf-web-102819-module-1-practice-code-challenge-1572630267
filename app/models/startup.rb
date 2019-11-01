5988c00079194927c11f567aa68469dd43055d4f
mwong068 <mwong068@gmail.com>
2019-11-01 12:01:07 -0700

    megans code challenge

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,55 @@
+# require 'pry'
+
 class Startup
 
+    #attr_accessor :name
+    attr_accessor :founder, :domain, :name
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+ 
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        founders_obj = Startup.all.find {|startup| startup.founder == founder_name}
+    end
+
+    def self.domains
+        domain_names = Startup.all.map {|startup| startup.domain}
+    end
+
+    def sign_contract(venture_capitalist, type, amount)
+        FundingRound.new(self, venture_capitalist, type, amount)
+    end
+
+    def funding_rounds
+        startup_rounds = FundingRound.all.map {|rounds| rounds.startup == self}
+        startup_rounds.length
+    end
+
+    def total_funds
+        total = 0
+        startup_rounds = FundingRound.all.select {|rounds| rounds.startup == self}
+        startup_rounds.each do |round|
+            round[]
+        total
+    end
+
 end
+
+
+
+# binding.pry
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,27 @@
+# require 'pry'
+
 class VentureCapitalist
 
+    attr_reader :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        rich_people = VentureCapitalist.all.select {|people| people.total_worth > 1000000000}
+        rich_people.map {|people| people.name}
+    end
+
 end
+
+
+# binding.pry
