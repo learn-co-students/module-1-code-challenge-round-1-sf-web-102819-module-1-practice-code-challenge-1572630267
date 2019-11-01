07bb5421fad6fa68aa0b22342a74f2ed33015e99
aryaziai <arya721@gmail.com>
2019-11-01 12:02:25 -0700

    arya challenge

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,43 @@
 class VentureCapitalist
 
+    
+    attr_reader :name 
+    attr_accessor :total_worth
+
+
+    @@all = []
+    
+    #create attributes for a venture capitalist dude
+    def initialize(name,total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+
+    #I created an array called rich_vc which has all rich people stored in
+    def self.tres_commas_club
+        rich_vc = @@all.select do |vc|
+            vc.total_worth > 1000000000
+        end
+        rich_vc
+    end
+
+
+
+    def self.all 
+        @@all
+    end
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,38 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+
+
+
+
+s1 = Startup.new("fb","mark","fb.com")
+s2 = Startup.new("google","joe","google.com")
+s3 = Startup.new("twitter","allen","twitter.com")
+
+
+vc1 = VentureCapitalist.new("Rich John", 10000000000)
+vc2 = VentureCapitalist.new("Rich Homie", 20000000000)
+vc3 = VentureCapitalist.new("Poor Man", 100)
+
+
+fr1 = FundingRound.new(s1,vc1,"Angel",100)
+fr2 = FundingRound.new(s1,vc2,"Seed",100)
+fr3 = FundingRound.new(s3,vc3,"Seed",1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
