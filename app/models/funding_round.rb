bef7e2712c72f843ffa031a8803572fa9d8c476a
github username <github email address>
2019-11-01 12:02:01 -0700

    adding my code

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,15 @@
 class FundingRound
+attr_reader :startup, :venture_capitalist, :type, :portfolio 
+attr_accessor :investment  
+@@all = []
+    
+    def initialize(startup)
+        @startup = startup
+    end
+
+    def investment
+        if @investment  # must be float and not negative
+        end
+    end
 
 end
